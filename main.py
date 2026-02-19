63d14551d252055f12183d7a228c09d1904d426c
emc-03 <emluna.dev@gmail.com>
2026-02-19 12:31:22 -0700

    Create clipboard handling and open browser link

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 #python3
 #mapIT.py
 import webbrowser, sys
+
+from pyperclip import paste
+
 if len(sys.argv) > 1:
     #Get Address from command line
     address = ''.join(sys.argv[1:])
-#TODO: Get address from clipboard
+else:
+    #Get Address from clipboard
+    address = paste()
+webbrowser.open('https://www.google.com/maps')
+
+
